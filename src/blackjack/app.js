687f462f50734155cb181f097c08d2ddfcff8d61
Suprimirx <andresfelipezapata2004@gmail.com>
2024-11-17 21:02:49 -0500

    Funcionamiento de la app

--- src/blackjack/app.js
+++ src/blackjack/app.js
@@ -1,4 +1,5 @@
 import _ from 'underscore';
+import html from './app.html?raw';
 import { crearDeck, pedirCarta, acumularPuntos, crearCarta, turnoComputadora } from './usecases';
 
 /*
@@ -6,45 +7,48 @@ import { crearDeck, pedirCarta, acumularPuntos, crearCarta, turnoComputadora } f
     * 2D = Two of Diamonds
     * 2H = Two of Hearts
     * 2S = Two of Spades
-    */
+*/
 
-export const App = () => {
+export const App = (elementId) => {
+
+    let deck = [];
+    const tipos = ['C', 'D', 'H', 'S'];
+    const especiales = ['A', 'J', 'Q', 'K'];
+
+    let puntosJugadores = [];
+
+    // Función para iniciar el juego.
+    const inicializarJuego = (numJugadores = 2) => {
+        deck = crearDeck(tipos, especiales);
+        puntosJugadores = [];
+        for(let i = 0; i < numJugadores; i++){
+            puntosJugadores.push(0);
+        }
+
+        // puntosHTML[0].innerText = 0;
+        // puntosHTML[1].innerText = 0;
+        puntosHTML.forEach(elem => elem.innerText = 0);
+
+        // divCartasJugadores[0].innerHTML = '';
+        // divCartasJugadores[1].innerHTML = ''; 
+        divCartasJugadores.forEach(elem => elem.innerHTML = '');
+
+        btnPedir.disabled = false;
+        btnDetener.disabled = false;
+    }
 
     (() => {
     
-        'use strict'
-        let deck = [];
-        const tipos = ['C', 'D', 'H', 'S'],
-            especiales = ['A', 'J', 'Q', 'K'];
-
-        let puntosJugadores = [];
-
-        //Función para iniciar el juego.
-        const inicializarJuego = (numJugadores = 2) => {
-            deck = crearDeck(tipos, especiales);
-            puntosJugadores = [];
-            for(let i = 0; i < numJugadores; i++){
-                puntosJugadores.push(0);
-            }
-
-            // puntosHTML[0].innerText = 0;
-            // puntosHTML[1].innerText = 0;
-            puntosHTML.forEach(elem => elem.innerText = 0);
-
-            // divCartasJugadores[0].innerHTML = '';
-            // divCartasJugadores[1].innerHTML = ''; 
-            divCartasJugadores.forEach(elem => elem.innerHTML = '');
-
-            btnPedir.disabled = false;
-            btnDetener.disabled = false;
-        }
+        const app = document.createElement('div');
+        app.innerHTML = html;
+        document.querySelector(elementId).append(app);
 
     })();
 
     // Referencias del HTML.  (#) es para referenciar el ID. (.) Para referenciar la clase.
-    const btnPedir = document.querySelector('#btnPedir'),
-        btnDetener = document.querySelector('#btnDetener'),
-        btnNuevo = document.querySelector('#btnNuevo');
+    const btnPedir = document.querySelector('#btnPedir')
+    const btnDetener = document.querySelector('#btnDetener')
+    const btnNuevo = document.querySelector('#btnNuevo');
     const divCartasJugadores = document.querySelectorAll('.divCartas');
     const puntosHTML = document.querySelectorAll('small');
 
@@ -75,6 +79,5 @@ export const App = () => {
     btnNuevo.addEventListener('click', () => {
         inicializarJuego();
     })
-
 
 }
